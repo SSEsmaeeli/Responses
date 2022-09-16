cb06d0da95ba4578eec9a3b1bd735c6519a0e153
seyyed sajjad esmaeeli <s.sajjad.esmaeeli@gmail.com>
2022-09-16 13:16:07 +0430

    install laravel breeze

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,25 +1,14 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<x-guest-layout>
+    <x-auth-card>
+        <x-slot name="logo">
+            <a href="/">
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            </a>
+        </x-slot>
 
-        <title>Laravel</title>
-    </head>
-    <body>
-        <h1>test</h1>
-        {{ $errors }}
-        <form action="{{ url('/store') }}" method="post">
-            @csrf
-            title: <input type="text" name="title" class="form-control">
-            body <input type="text" name="body">
-            <button type="submit">Send</button>
-        </form>
+        <h1 class="mb-4">Welcome to blog</h1>
 
-        @isset($post)
-            {{ $post->id }}<br>
-            {{ $post->title }}<br>
-            {{ $post->created_at }}<br>
-        @endif
-    </body>
-</html>
+       <a href="{{ route('register') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Register</a>
+       <a href="{{ route('login') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Login</a>
+    </x-auth-card>
+</x-guest-layout>
